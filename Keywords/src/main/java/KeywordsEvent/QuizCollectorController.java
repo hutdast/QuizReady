f7231610a09404cc9f66a9cf28560f3bf475ba63
hutdast <mayhem@nikensons-iMac.local>
2016-01-02 18:17:39 -0500

    removeExam module created
    The removeExam module enables the removal of a set of sentence and keys
    in the tabular presentation of the exam during and after its creation.

--- Keywords/src/main/java/KeywordsEvent/QuizCollectorController.java
+++ Keywords/src/main/java/KeywordsEvent/QuizCollectorController.java
@@ -29,7 +29,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
@@ -37,158 +36,168 @@ import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "QC")
 @SessionScoped
-public class QuizCollectorController extends DataManagement 
+public class QuizCollectorController extends DataManagement
 {
-
+    
     private Serviceable quiz;
     private List<Quiz> quizzes;
     @ManagedProperty(value = "#{QI}")
     private QuizInfoController infoBean;
     private String quizID;
-  
+    
     @PostConstruct
     public void init()
-    {  System.out.println("init QC is invoked and isNewQuiz() is  ..:"+infoBean.isExistingQuiz());
-        this.quiz = AppOps.getModel(AppModels.QUIZ);
+    {
+        quiz = AppOps.getModel(AppModels.QUIZ);
+        infoBean.getQuiz().setBundle(new HashMap<>());//Initialize the infobean quiz table
         if(infoBean.isExistingQuiz() == true)
-        {  
-            getExistingQuiz(); 
+        {
+            getExistingQuiz();
         }else
         {
-           this.quizzes =  new ArrayList<>();  
+            quizzes =  new ArrayList<>();
         }
-
+        
     }
-
-    public String reinit() 
+    
+    public String reinit()
     {
-        System.out.println(" From QC reinit the value of getQuizID()..:");
-        saveToDB(quiz.getSentence(), quiz.getKeys());
+        screenForDB(quiz.getSentence(), quiz.getKeys());
         quiz = AppOps.getModel(AppModels.QUIZ);
         return null;
     }
-
-    public List<Quiz> getQuizzes() 
+    
+    public List<Quiz> getQuizzes()
     {
         return quizzes;
     }
-
+    
     public void setQuizzes(List<Quiz> quizzes)
     {
         this.quizzes = quizzes;
     }
-
-    public Serviceable getQuiz() 
+    
+    public Serviceable getQuiz()
     {
         return quiz;
     }
-
-    public void setQuiz(Serviceable quiz) 
+    
+    public void setQuiz(Serviceable quiz)
     {
         this.quiz = quiz;
     }
-
+    
     public QuizInfoController getInfoBean()
     {
         return infoBean;
     }
-
+    
     public void setInfoBean(QuizInfoController infoBean)
     {
         this.infoBean = infoBean;
     }
-
-    public String getQuizID() 
+    
+    public String getQuizID()
     {
         return quizID;
     }
-
+    
     public void setQuizID(String quizID)
     {
         this.quizID = quizID;
     }
-
-//****************** prepBundle(String sentence, String keys) *********************
-//================================================================================= 
+    
+    //****************** screenForDB(String sentence, String keys)  *********************
+    //=================================================================================
     /**
-     * prepBundle(String sentence, String keys) prepare the question set before
-     * it enters the mongoDB.
+     * screenForDB(String sentence, String keys)  Save the question set before
+     * it enters the mongoDB and alert user about empty fields and requirement
+     * violations.
      *
      * @param sentence
      * @param keys
      */
-    private void saveToDB(String sentence, String keys) 
+    private void screenForDB(String sentence, String keys)
     {
         String delimeter = "\\s|,|\\.";
-        for (String key : keys.split(delimeter)) 
+        for (String key : keys.split(delimeter))
         {
-
-            if (!sentence.contains(key.trim())) 
+            
+            if (!sentence.contains(key.trim()))
             {
-
+                
                 addMessage("msgs1", " The key " + key + " doesn't match any word in the corresponding sentence",
-                        "Mismatch!");
+                           "Not saved!");
                 return;
             } else if (sentence.equals("") || key.equals(""))
             {
+                addMessage("msgs1", "Sentence and keys cannot be left empty",
+                           "Not saved!");
                 return;//prevent empty entries into the database
-
-            } else 
+                
+            } else
             {
+                
                 infoBean.getQuiz().getBundle().put(sentence, keys);
                 mongoStore((Quiz) infoBean.getQuiz());
             }
         }//end for loop String key
-
+        
     }
-  
-private void getExistingQuiz()
- {
-     String id = infoBean.getQuiz().getId();
-     this.quizzes = mongoQueryQ(id).getBundle().entrySet().stream()
-                .map(e -> 
-                {
-                    return new Quiz(quiz.getId(),e.getKey(), e.getValue());
-                })
-                .collect(Collectors.toList());
-                
-              
- }    
+    //****************** getExistingQuiz() ********************************************
+    //=================================================================================
+    /**
+     * getExistingQuiz() captures the bundle from the quiz that is in the database
+     * (if one is found from infobean) then disassembles it into several quiz objects.
+     *
+     */
+    private void getExistingQuiz()
+    {
+        String id = infoBean.getQuiz().getId();
+        if(mongoQueryQ(id).getBundle().isEmpty())
+        {
+            System.out.print(" ... ");
+            return;
+        }  else
+        {
+            quizzes = mongoQueryQ(id).getBundle().entrySet().stream()
+            .map(e ->
+                 {
+                     return new Quiz(quiz.getId(),e.getKey(), e.getValue());
+                 })
+            .collect(Collectors.toList());
+        }
+        
+        
+        
+    }
     
-//****************** prepBundle(String sentence, String keys) *********************
-//================================================================================= 
+    //****************** removeExam(Quiz q) *******************************************
+    //=================================================================================
     /**
      * prepBundle(String sentence, String keys) prepare the question set before
      * it enters the mongoDB.
      *
      * @param q
      */
-    /**
-     * public void removeExam(Quiz q) { System.out.print("removeExam()
-     * invoked:.. ");
-     *
-     * if (storedQuiz.getBundle().containsKey(q.getSentence()) ) {
-     *
-     *
-     * int max = quizs.size(); storedQuiz.getBundle().remove(q.getSentence());
-     *
-     * quizs = storedQuiz.getBundle().entrySet().stream() .map(e -> {
-     *
-     * return new Quiz(storedQuiz.getId(), e.getKey(), e.getValue()); })
-     * .collect(Collectors.toList());
-     *
-     * //quizs.remove(rowID);
-     *
-     * System.out.print("Inside quizs:.. " + quizs.size());
-     * System.out.print("Inside q:.. " + q.toString()); System.out.print("size
-     * of temp:.. " + temp.size()); System.out.print("size of storedQuiz:.. " +
-     * storedQuiz.getBundle().size());
-     *
-     * }else{ return; }
-     *
-     * System.out.print("leaving removeExam():.. ");
-     *
-     * }
-     *
-     */
+    
+    public void removeExam(Quiz q)
+    {
+        
+        System.out.print("removeExam() is ivoked, and the new list is..:");
+        quizzes =   quizzes.stream()
+        .filter(e -> !(e.getSentence().equals(q.getSentence())))
+        .collect(Collectors.toList());
+        infoBean.getQuiz().getBundle().remove(q.getSentence(), q.getKeys());
+        mongoStore((Quiz) infoBean.getQuiz());
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
 }//end of QuizCollectorEvent
